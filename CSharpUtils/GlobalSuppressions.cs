72f6af570ebd9581e55ac1b823faeeb0534bcbb9
Neil Justice <njjustice@gmail.com>
2022-07-28 15:40:50 -0500

    Remove unused folders CSharpUtils and CSharpUtilsTests

--- CSharpUtils/GlobalSuppressions.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces")]
-[assembly: SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize")]
-[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression")]
--- CSharpUtilsTests/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces")]
-[assembly: SuppressMessage("Style", "IDE0034:Simplify 'default' expression")]
-[assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization")]
-[assembly: SuppressMessage("Usage", "CA1816:Dispose methods should call SuppressFinalize")]
