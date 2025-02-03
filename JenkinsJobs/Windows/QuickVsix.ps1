2aac20229ff2b0256e0e56338159a233844c61ee
Neil Justice <njjustice@gmail.com>
2025-02-03 13:20:43 -0600

    Updates

--- JenkinsJobs/Windows/QuickVsix.ps1
+++ JenkinsJobs/Windows/QuickVsix.ps1
@@ -3,5 +3,6 @@ param($doInstall = "true")
 WindowsCSharpBuilder.exe build-csharp-program `
    --solution-name=QuickVsix `
    --configuration=Release `
-   --install=$doInstall
+   --install=$doInstall `
+   --install-with-release-folder-name-suffix
 exit $LastExitCode
