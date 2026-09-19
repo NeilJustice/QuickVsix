4300764ff7099a17f3507023a4719ffa1af86af7
Neil Justice <njjustice@gmail.com>
2026-09-19 16:12:18 -0500

    Update build script args from 'solution-name' to 'solution'

--- JenkinsJobs/Windows/QuickVsix.ps1
+++ JenkinsJobs/Windows/QuickVsix.ps1
@@ -1,7 +1,7 @@
 param($doInstall = "true")
 
 WindowsCSharpBuilder.exe build-csharp-solution `
-   --solution-name=QuickVsix `
+   --solution=QuickVsix `
    --configuration=Release `
    --install=$doInstall
 exit $LastExitCode
